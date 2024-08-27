b3a8e0cee4a125acc54b68b075e8f0365220ef84
MagicienPY <juniornote7@gmail.com>
2024-08-27 02:51:49 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php 
+require_once 'php_action/db_connect.php';
+
+session_start();
+
+if(isset($_SESSION['userId'])) {
+	header('location:dashboard.php');	
+}
+
+$errors = array();
+
+if($_POST) {		
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(empty($username) || empty($password)) {
+		if($username == "") {
+			$errors[] = "Username is required";
+		} 
+
+		if($password == "") {
+			$errors[] = "Password is required";
+		}
+	} else {
+$sql="select * from users where username='$username' and password='$password'; ";
+$result=mysqli_query($connect,$sql);
+$count=mysqli_num_rows($result);
+$row=mysqli_fetch_array($result);
+
+if ($row>1) { 
+	   $_SESSION['userId']=$row['username'];
+		header("location:dashboard.php");
+}
+ else {
+	?><script type="text/javascript">
+		alert("Wrong details!!");
+		window.location= "index.php";
+	</script>
+<?php
+}
+
+	} // 
+	
+} // /if $_POST
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>STOCKY Delfran</title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+	<!-- bootstrap theme-->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap-theme.min.css">
+	<!-- font awesome -->
+	<link rel="stylesheet" href="assests/font-awesome/css/font-awesome.min.css">
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="custom/css/custom.css">	
+
+  <!-- jquery -->
+	<script src="assests/jquery/jquery.min.js"></script>
+  <!-- jquery ui -->  
+  <link rel="stylesheet" href="assests/jquery-ui/jquery-ui.min.css">
+  <script src="assests/jquery-ui/jquery-ui.min.js"></script>
+
+  <!-- bootstrap js -->
+	<script src="assests/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row vertical">
+			<div class="col-md-5 col-md-offset-4">
+				<div class="panel panel-info">
+					
+					<div class="panel-heading">
+						<h3 class="panel-title">Svp connectez vous</h3>
+					</div>
+					<div class="panel-body">
+
+						<div class="messages">
+							<?php if($errors) {
+								foreach ($errors as $key => $value) {
+									echo '<div class="alert alert-warning" role="alert">
+									<i class="glyphicon glyphicon-exclamation-sign"></i>
+									'.$value.'</div>';										
+									}
+								} ?>
+						</div>
+
+						<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
+							<fieldset>
+							  <div class="form-group">
+									<label for="username" class="col-sm-2 control-label">Nom d'utilisateur</label>
+									<div class="col-sm-10">
+									  <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" />
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password" class="col-sm-2 control-label">Mot de passe</label>
+									<div class="col-sm-10">
+									  <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" />
+									</div>
+								</div>								
+								<div class="form-group">
+									<div class="col-sm-offset-2 col-sm-10">
+									  <button type="submit" class="btn btn-default"> <i class="glyphicon glyphicon-log-in"></i> Se connecter</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<!-- panel-body -->
+				</div>
+				<!-- /panel -->
+			</div>
+			<!-- /col-md-4 -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- container -->	
+</body>
+</html>
+
+
+
+
+
+
+
+	
